948dc57a79b0d98a42a406f5d1e24c6b25e8efc6
Matija Nalis <mnalis-git@voyager.hr>
2020-10-18 23:19:39 +0200

    consolidate calculating time difference

--- list.js
+++ list.js
@@ -22,17 +22,30 @@
 
 const elemSecList = document.getElementById('sec_list');
 const elemTableList = document.getElementById('table_body');
+const elemNewTask = document.getElementById('btn_new_task');
+const elemShowTotals = document.getElementById('btn_totals');
 
-/* returns difference between start and end times, taking into account possible midnight wrap */
-function time_diff(start, end) {
-    let [s_h, s_m] = start.split(":");
-    let [e_h, e_m] = end.split(":");
+/* returns number of minutes worked between start and end, 
+   taking into account possible midnight wrap and break times */
+function calc_minutes(start, end, breaks) {
+    let [s_h, s_m] = start.split(':');
+    let [e_h, e_m] = end.split(':');
+    let [b_h, b_m] = end.split(':');
 
     let s_total = Number(s_h) * 60 + Number(s_m);
     let e_total = Number(e_h) * 60 + Number(e_m);
+    let b_total = Number(b_h) * 60 + Number(b_m);
 
     let diff_min = e_total - s_total;
     if (diff_min < 0) diff_min += 24*60;			 	// handle midnight wrap
+    diff_min -= b_total;
+    return diff_min;
+}
+
+/* returns human readable difference between start and end times, excluding break times */
+function time_diff(start, end, breaks) {
+    let diff_min = calc_minutes(start, end, breaks);
+    if (diff_min < 0) console.error ('worked less than 0 minutes: ' + diff_min)
 
     let diff_h = Math.floor ( diff_min / 60 );
     let diff_m = diff_min % 60;
@@ -50,8 +63,8 @@ function build_list(month) {
         rows += '<td>' + task[0] + '</td>';			// date
         rows += '<td>' + task[1] + '</td>';			// start time
         rows += '<td>' + task[2] + '</td>';			// end time
-        rows += '<td>' + time_diff(task[1], task[2]) + '</td>';	// time worked
-        rows += '<td>' + task[3] + '</td>';			// notes
+        rows += '<td>' + time_diff(task[1], task[2], task[3]) + '</td>';	// time worked: end-start-breaks
+        rows += '<td>' + task[4] + '</td>';			// notes
         rows += '</tr> ';
     }
 
@@ -64,5 +77,5 @@ function show_list(month) {
     //console.debug ('table_body before:', elemTableList.innerHTML);
     elemTableList.innerHTML = build_list(month);
     //console.debug ('table_body after:', elemTableList.innerHTML);
-    elemSecList.style.display = "block";			// unhide table
+    elemSecList.style.display = 'block';			// unhide table
 }
