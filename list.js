d127b75e6162a4a734b5e0e50cc9e338e6a57cef
Matija Nalis <mnalis-git@voyager.hr>
2020-10-18 23:23:22 +0200

    consolidates time_format

--- list.js
+++ list.js
@@ -25,7 +25,7 @@ const elemTableList = document.getElementById('table_body');
 const elemNewTask = document.getElementById('btn_new_task');
 const elemShowTotals = document.getElementById('btn_totals');
 
-/* returns number of minutes worked between start and end, 
+/* returns number of minutes worked between start and end,
    taking into account possible midnight wrap and break times */
 function calc_minutes(start, end, breaks) {
     let [s_h, s_m] = start.split(':');
@@ -37,9 +37,14 @@ function calc_minutes(start, end, breaks) {
     let b_total = Number(b_h) * 60 + Number(b_m);
 
     let diff_min = e_total - s_total;
-    if (diff_min < 0) diff_min += 24*60;			 	// handle midnight wrap
+    if (diff_min < 0) diff_min += 24*60;	 	// handle midnight wrap
     diff_min -= b_total;
     return diff_min;
+}
+
+// zeroleads the hours:minutes
+function format_time(h, m) {
+    return ('0'+h).slice(-2) + ':' + ('0'+m).slice(-2);
 }
 
 /* returns human readable difference between start and end times, excluding break times */
@@ -50,7 +55,7 @@ function time_diff(start, end, breaks) {
     let diff_h = Math.floor ( diff_min / 60 );
     let diff_m = diff_min % 60;
 
-    return ('0'+diff_h).slice(-2) + ':' + ('0'+diff_m).slice(-2);	// zeroleads the hours:minutes
+    return format_time(diff_h, diff_m);
 }
 
 /* build table rows for specified months */
